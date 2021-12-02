c488bc25e58d830e26ff3513c18ac7e362e94075
Andy_Dragon <66428908+neveralone-dragon@users.noreply.github.com>
2021-12-02 23:50:38 +0800

    Update 13.罗马数字转整数.py

--- simple/1-50/13.罗马数字转整数.py
+++ simple/1-50/13.罗马数字转整数.py
@@ -1,1 +1,28 @@
-def:
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        num = 0
+        for i in range(len(s)):
+            if s[i] == 'M':
+                num+=1000
+            elif s[i] == 'D':
+                num+=500
+            elif s[i] == 'C':
+                if i+1<len(s) and (s[i+1] == 'D'or s[i+1]=='M'):
+                    num-=100
+                else:
+                    num+=100
+            elif s[i] == 'L':
+                num+=50
+            elif s[i] == 'X':
+                if i+1<len(s) and (s[i+1] == 'L'or s[i+1]=='C'):
+                    num-=10
+                else:
+                    num+=10
+            elif s[i] == 'V':
+                num+=5
+            elif s[i] == 'I':
+                if i+1 < len(s) and (s[i+1] == 'V' or s[i+1] == 'X'):
+                    num-=1
+                else:
+                    num+=1
+        return num
