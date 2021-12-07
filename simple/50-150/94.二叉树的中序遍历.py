6cde9013a38895babe5d63aacd97f4e65774c979
Andy_Dragon <66428908+neveralone-dragon@users.noreply.github.com>
2021-12-07 22:59:00 +0800

    Create 94.二叉树的中序遍历.py

---
+++ simple/50-150/94.二叉树的中序遍历.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        output = []
+        def inorder_traversal(root, output):
+            if root:
+                if root.left:
+                    inorder_traversal(root.left,output)
+                output.append(root.val)
+                if root.right:
+                    inorder_traversal(root.right,output)
+        inorder_traversal(root,output)
+        return output
+
+      
+# 感谢@自来火     
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        stack,rst = [root],[]
+        while stack:
+            i = stack.pop()
+            if isinstance(i,TreeNode):
+                stack.extend([i.right,i.val,i.left])
+            elif isinstance(i,int):
+                rst.append(i)
+        return rst   
+
+      
+# 感谢@hzhu212      
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        WHITE, GRAY = 0, 1
+        res = []
+        stack = [(WHITE, root)]
+        while stack:
+            color, node = stack.pop()
+            if node is None: continue
+            if color == WHITE:
+                stack.append((WHITE, node.right))
+                stack.append((GRAY, node))
+                stack.append((WHITE, node.left))
+            else:
+                res.append(node.val)
+        return res
+
+# 作者：hzhu212
+# 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal/solution/yan-se-biao-ji-fa-yi-chong-tong-yong-qie-jian-ming/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。      
